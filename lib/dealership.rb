a0c0e96effcf0613a7a47f70e300d65c8080b691
Isaac Thill <ithill22@turing.edu>
2023-02-08 20:18:15 -0700

    Add completion of all iteration 4 but inventory_hash

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -8,7 +8,6 @@ class Dealership
     @address = address
     @inventory = []
     @inventory_count = 0
-
   end
 
   def add_car(car)
@@ -31,5 +30,19 @@ class Dealership
   def details
     {total_value: total_value, address: @address}
   end
+
+  def average_price_of_car
+    (total_value).div(inventory_count).to_s.reverse.scan(/\d{3}|.+/).join(",").reverse
+  end
+
+  def cars_sorted_by_price
+    @inventory.sort_by{|car| car.total_cost}
+  end
+
+  def inventory_hash
+   makes = @inventory.filter{|car| car.make == make}
+  end
+
+
 end
    
