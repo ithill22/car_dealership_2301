a7d9a58a3a930b7270164ff02199abad67049ea5
Isaac Thill <ithill22@turing.edu>
2023-02-08 17:35:44 -0700

    Add solutions up to total_value

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -15,5 +15,17 @@ class Dealership
     @inventory << car
     @inventory_count = inventory.count
   end
+
+  def inventory?
+    @inventory_count > 0
+  end
+
+  def cars_by_make(make)
+    @inventory.filter{|car| car.make == make}
+  end
+
+  def total_value
+    @inventory.map.sum{|car| car.total_cost}
+  end
 end
    
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -44,4 +44,50 @@ RSpec.describe Dealership do
 
   end
 
+  it 'can determine if it has an inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+
+    expect(dealership.inventory?).to eq(false)
+
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.inventory?).to eq(true)
+  end
+
+  it 'can list cars by make' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.cars_by_make('Toyota')).to eq([car_2, car_3])
+    expect(dealership.cars_by_make('Ford')).to eq([car_1])
+    expect(dealership.cars_by_make('Chevrolet')).to eq([car_4])
+  end
+
+  it 'can list the total value of its inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.total_value).to eq(156000)
+
+  end
+
 end
